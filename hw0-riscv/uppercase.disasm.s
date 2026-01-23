3d858021c36f2c9761748066cd2a0b6e853a475b
anushkaj <anushkaj@seas.upenn.edu>
2026-01-23 12:15:20 +0530

    update on hw1a

--- hw0-riscv/uppercase.disasm.s
+++ hw0-riscv/uppercase.disasm.s
@@ -5,21 +5,22 @@ uppercase.bin:     file format elf32-littleriscv
 Disassembly of section .text:
 
 00010074 <_start>:
-   10074:	00002537          	lui	a0,0x2
+   10074:	ffff2517          	auipc	a0,0xffff2
+   10078:	f8c50513          	addi	a0,a0,-116 # 2000 <__DATA_BEGIN__>
 
-00010078 <loop>:
-   10078:	00050283          	lb	t0,0(a0) # 2000 <__DATA_BEGIN__>
-   1007c:	02028263          	beqz	t0,100a0 <end_program>
-   10080:	06100313          	li	t1,97
-   10084:	0062ca63          	blt	t0,t1,10098 <skip>
-   10088:	07a00313          	li	t1,122
-   1008c:	00534663          	blt	t1,t0,10098 <skip>
-   10090:	fe028293          	addi	t0,t0,-32
-   10094:	00550023          	sb	t0,0(a0)
+0001007c <loop>:
+   1007c:	00050283          	lb	t0,0(a0)
+   10080:	02028263          	beqz	t0,100a4 <done>
+   10084:	06100313          	li	t1,97
+   10088:	0062ca63          	blt	t0,t1,1009c <next>
+   1008c:	07a00313          	li	t1,122
+   10090:	00534663          	blt	t1,t0,1009c <next>
+   10094:	fe028293          	addi	t0,t0,-32
+   10098:	00550023          	sb	t0,0(a0)
 
-00010098 <skip>:
-   10098:	00150513          	addi	a0,a0,1
-   1009c:	fddff06f          	j	10078 <loop>
+0001009c <next>:
+   1009c:	00150513          	addi	a0,a0,1
+   100a0:	fddff06f          	j	1007c <loop>
 
-000100a0 <end_program>:
-   100a0:	0000006f          	j	100a0 <end_program>
+000100a4 <done>:
+   100a4:	0000006f          	j	100a4 <done>
